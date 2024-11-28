f5e3cb27b32cd1451ce61e87242f84a31d2c676f
Jean <jean.roodrigues@outlook.com>
2024-11-28 14:16:09 -0300

    Subindo Controllers

--- src/main/java/com/estudo/ProjDog/controllers/DonoController.java
+++ src/main/java/com/estudo/ProjDog/controllers/DonoController.java
@@ -1,5 +1,105 @@
 package com.estudo.ProjDog.controllers;
 
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudo.ProjDog.entity.Dono;
+import com.estudo.ProjDog.services.DonoService;
+
+import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/dono")
 public class DonoController {
     
+    @Autowired
+    private DonoService donoService;
+    
+    @GetMapping("/all")
+    public ResponseEntity<List<Dono>> getDonos() {
+        List<Dono> donos = donoService.getAllDonos();
+        if (donos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else{
+            return new ResponseEntity<>(donos, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Dono> getDonoById(@PathVariable Long id) {
+        Optional<Dono> dono = donoService.getDonoById(id);
+        if (dono.isPresent()) {
+            return new ResponseEntity<>(dono.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{nome}")
+    public ResponseEntity<List<Dono>> getDonosByName(@PathVariable String nome) {
+        List<Dono> donos = donoService.getDonosByName(nome);
+        if (donos.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(donos, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/paciente/{id_paciente}")
+    public ResponseEntity<Dono> getDonoByPacienteId(@PathVariable Long id_paciente) {
+        try {
+            Dono dono = donoService.getDonoByPacienteId(id_paciente);
+            return ResponseEntity.ok(dono);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+    
+    @PostMapping("/save")
+    public ResponseEntity<Dono> saveDono(@RequestBody Dono dono) {
+        Dono donoObj = donoService.saveDono(dono);
+        return new ResponseEntity<>(donoObj, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Dono> updateDonoById(@PathVariable Long id, @RequestBody Dono newDonoData) {
+        Dono donoUpdate = donoService.updateDonoById(id, newDonoData);
+        if (donoUpdate != null) { 
+            return new ResponseEntity<>(donoUpdate, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteDonoById(@PathVariable Long id) {
+        donoService.deleteDonoById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    
+
 }
--- src/main/java/com/estudo/ProjDog/controllers/PacienteController.java
+++ src/main/java/com/estudo/ProjDog/controllers/PacienteController.java
@@ -1,5 +1,99 @@
 package com.estudo.ProjDog.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estudo.ProjDog.entity.Paciente;
+import com.estudo.ProjDog.services.PacienteService;
+
+import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/paciente")
 public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Paciente>> getPacientes(){
+        List<Paciente> pacientes = pacienteService.getAllPacientes();
+        if (pacientes.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else{
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Paciente> getPacienteById(@PathVariable Long id) {
+        Optional<Paciente> paciente = pacienteService.getPacienteById(id);
+        if (paciente.isPresent()) {
+            return new ResponseEntity<>(paciente.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
     
+    @GetMapping("/{nome}")
+    public ResponseEntity<List<Paciente>> getPacienteByName(@PathVariable String nome) {
+        List<Paciente> pacientes = pacienteService.getPacientesByName(nome);
+        if (pacientes.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(pacientes, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/dono/{id_dono}")
+    public ResponseEntity<List<Paciente>> getPacientesByDonoId(@PathVariable Long id_dono) {
+        try {
+            List<Paciente> pacientes = pacienteService.getPacientesByDonoId(id_dono);
+            return ResponseEntity.ok(pacientes);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Paciente> savePaciente(@RequestBody Paciente paciente) {
+        Paciente pacienteObj = pacienteService.savePaciente(paciente);
+        return new ResponseEntity<>(pacienteObj, HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Paciente> updatePacienteById(@PathVariable Long id, @RequestBody Paciente newPacienteData) {
+        Paciente pacienteUpdate = pacienteService.updatePacienteById(id, newPacienteData);
+        if (pacienteUpdate != null) { 
+            return new ResponseEntity<>(pacienteUpdate, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deletePacienteById(@PathVariable Long id) {
+        pacienteService.deleteByPacienteId(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
